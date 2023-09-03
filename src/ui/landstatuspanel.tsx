fcf0caf0cbe56b487f8cb9d7f8798299b1fe2118
job <hujunjob@qq.com>
2023-09-03 18:54:19 +0800

    Update landstatuspanel.tsx

--- src/ui/landstatuspanel.tsx
+++ src/ui/landstatuspanel.tsx
@@ -1,7 +1,8 @@
 export default function LandStatusPanel(){
     return(<div>
-        <div style={{width:200,height:130,lineHeight:1, backgroundColor:"rgba(0, 0, 0, 0.5)",padding:10, borderRadius:15}}>
-            <p>Bank</p>
+        <div style={{width:200,height:140,lineHeight:1, backgroundColor:"rgba(0, 0, 0, 0.5)",padding:10, borderRadius:15}}>
+            <p>Current Land</p>
+            <p>Building : Bank</p>
             <p>Owner : 0x000</p>
             <p>Price : $100</p>
         </div>
