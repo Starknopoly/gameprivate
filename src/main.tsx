058fcc28e257755b9bc7b209c02444727614fd81
job <hujunjob@qq.com>
2023-09-22 10:56:58 +0800

    Add: Remove log in production

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,9 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("React root not found");
 const root = ReactDOM.createRoot(rootElement);
 
+if (process.env.NODE_ENV === 'production') {
+    console.log = function() {};
+    console.warn = function() {};
+    console.error = function() {};
+}
 root.render(<App />);
